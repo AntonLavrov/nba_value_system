68ee14bc1269bb98d9d4b176f15e6e597e827dc3
AntonLavrov <anton_nj@mail.ru>
2025-11-25 01:32:00 +0300

    Update expected.py

--- core/model/expected.py
+++ core/model/expected.py
@@ -2,41 +2,61 @@
 
 from core.context import GameContext
 
+
 class ExpectedModel:
     def process(self, context: GameContext):
         """
-        Перенесена логика из model_ev.py и model_expectations.py
+        Интегрированная модель ожидаемого диффа.
+
+        Использует:
+        - базовые рейтинги команд (rating_home / rating_away)
+        - усталость (fatigue_home / fatigue_away)
+        - состав (lineup_home / lineup_away)
+        - мотивацию (motivation_home / motivation_away)
+        - shot quality (shot_quality_delta)
+        - темп (pace_match vs league_avg_pace)
         """
 
-        # пример: базовая оценка команды
+        # ---- базовый дифф по рейтингу ----
         rating_home = context.features.get("rating_home", 0.0)
         rating_away = context.features.get("rating_away", 0.0)
         home_court_adv = context.features.get("home_court_adv", 0.0)
 
         base_diff = rating_home - rating_away + home_court_adv
 
-        # учёт усталости
+        # ---- усталость ----
         fatigue_home = context.features.get("fatigue_home", 0.0)
         fatigue_away = context.features.get("fatigue_away", 0.0)
         fatigue_adj = fatigue_home - fatigue_away
 
-        # учёт состава
+        # ---- состав / травмы ----
         lineup_home = context.features.get("lineup_home", 0.0)
         lineup_away = context.features.get("lineup_away", 0.0)
         lineup_adj = lineup_home - lineup_away
 
-        # учёт мотивации
+        # ---- мотивация ----
         motivation_home = context.features.get("motivation_home", 0.0)
         motivation_away = context.features.get("motivation_away", 0.0)
         motivation_adj = motivation_home - motivation_away
 
-        # учёт темпа (может влиять на дифф)
-        pace = context.features.get("pace", 0.0)
+        # ---- shot quality (xPTS) ----
+        shot_quality_delta = context.features.get("shot_quality_delta", 0.0)
 
-        expected_diff = base_diff + fatigue_adj + lineup_adj + motivation_adj
+        # ---- темп ----
+        pace_match = context.features.get("pace_match", 0.0)
+        league_avg_pace = context.features.get("league_avg_pace", 0.0)
+        pace_factor = 1.0
+        if pace_match > 0 and league_avg_pace > 0:
+            pace_factor = pace_match / league_avg_pace
 
-        # если хочешь, темп корректирует коэффициент
-        if pace:
-            expected_diff *= (pace / 100.0)
+        # ---- финальный ожидаемый дифф ----
+        expected_diff = base_diff
+        expected_diff += fatigue_adj
+        expected_diff += lineup_adj
+        expected_diff += motivation_adj
+        expected_diff += shot_quality_delta
+
+        # мягко учитываем темп (опционально)
+        expected_diff *= pace_factor
 
         context.set_output("expected_diff", expected_diff)
